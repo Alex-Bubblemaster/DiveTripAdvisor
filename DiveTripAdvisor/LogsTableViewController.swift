09b941e4d2576b72999ba8aa559b8d5cec6961cf
Alex-Bubblemaster <shiatzu@gmail.com>
2017-05-13 20:01:56 +0300

    feat: add detail to log vc

--- DiveTripAdvisor/LogsTableViewController.swift
+++ DiveTripAdvisor/LogsTableViewController.swift
@@ -36,15 +36,7 @@ class LogsTableViewController: UITableViewController, HttpRequesterDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "log-cell")
-        
         self.loadLogs()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     func didReceiveData(data: Any) {
@@ -68,7 +60,6 @@ class LogsTableViewController: UITableViewController, HttpRequesterDelegate {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Table view data source
@@ -83,59 +74,15 @@ class LogsTableViewController: UITableViewController, HttpRequesterDelegate {
         return self.logs.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "log-cell", for: indexPath)
-        cell.textLabel?.text = self.logs[indexPath.row].site
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "log-cell", for: indexPath) as! LogsTableViewCell
+        cell.siteName?.text = self.logs[indexPath.row].site
+        cell.siteName?.layer.cornerRadius = 5
+        cell.depth?.text = String(self.logs[indexPath.row].depth!) + " m"
+        cell.time?.text = String(self.logs[indexPath.row].time!) + " mins"
+        cell.locationName?.text = self.logs[indexPath.row].location
         // Configure the cell...
         
         return cell
     }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
---
+++ DiveTripAdvisor/ViewControllers/LogsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  LogsTableViewCell.swift
+//  DiveTripAdvisor
+//
+//  Created by Alexandrina Hajigeorgieva on 13/05/2017.
+//  Copyright © 2017 Alexandrina Hajigeorgieva. All rights reserved.
+//
+
+import UIKit
+
+class LogsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var locationName: UILabel!
+    @IBOutlet weak var siteName: UILabel!
+    @IBOutlet weak var depth: UILabel!
+    @IBOutlet weak var time: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
